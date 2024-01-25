876fe1c263084f69db13556a3b0de7757fb7b5d9
Axel Sianturi <sianturiaxel@gmail.com>
2024-01-25 20:16:41 +0700

    update controler,model,view

--- backend/controllers/JurusanController.php
+++ backend/controllers/JurusanController.php
@@ -3,18 +3,15 @@
 namespace backend\controllers;
 
 use backend\models\Jurusan;
-use backend\models\JurusanMapel;
-use backend\models\JurusanMapelSearch;
-use backend\models\MataPelajaran;
-use yii\helpers\ArrayHelper;
+use backend\models\JurusanSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 
 /**
- * JurusanMapelController implements the CRUD actions for JurusanMapel model.
+ * JurusanController implements the CRUD actions for Jurusan model.
  */
-class JurusanMapelController extends Controller
+class JurusanController extends Controller
 {
     /**
      * @inheritDoc
@@ -35,68 +32,46 @@ class JurusanMapelController extends Controller
     }
 
     /**
-     * Lists all JurusanMapel models.
+     * Lists all Jurusan models.
      *
      * @return string
      */
     public function actionIndex()
     {
-        $searchModel = new JurusanMapelSearch();
+        $searchModel = new JurusanSearch();
         $dataProvider = $searchModel->search($this->request->queryParams);
-
-        $jurusanMapelQuery = JurusanMapel::find()->with('jurusan', 'mataPelajaran');
-
-        $jurusanMapelData = [];
-
-        foreach ($jurusanMapelQuery->all() as $jm) {
-            $jurusanNama = $jm->jurusan ? $jm->jurusan->nama : 'Data tidak tersedia';
-            $mapelNama = $jm->mataPelajaran ? $jm->mataPelajaran->name : 'Data tidak tersedia';
-
-            if (!array_key_exists($jm->jurusan_id, $jurusanMapelData)) {
-                $jurusanMapelData[$jm->jurusan_id] = [
-                    'nama' => $jurusanNama,
-                    'mapel' => []
-                ];
-            }
-            $jurusanMapelData[$jm->jurusan_id]['mapel'][] = $mapelNama;
-        }
 
         return $this->render('index', [
             'searchModel' => $searchModel,
             'dataProvider' => $dataProvider,
-            'jurusanMapelData' => $jurusanMapelData,
         ]);
     }
 
-
-
-
-
     /**
-     * Displays a single JurusanMapel model.
-     * @param int $jurusan_mapel_id Jurusan Mapel ID
+     * Displays a single Jurusan model.
+     * @param int $jurusan_id Jurusan ID
      * @return string
      * @throws NotFoundHttpException if the model cannot be found
      */
-    public function actionView($jurusan_mapel_id)
+    public function actionView($jurusan_id)
     {
         return $this->render('view', [
-            'model' => $this->findModel($jurusan_mapel_id),
+            'model' => $this->findModel($jurusan_id),
         ]);
     }
 
     /**
-     * Creates a new JurusanMapel model.
+     * Creates a new Jurusan model.
      * If creation is successful, the browser will be redirected to the 'view' page.
      * @return string|\yii\web\Response
      */
     public function actionCreate()
     {
-        $model = new JurusanMapel();
+        $model = new Jurusan();
 
         if ($this->request->isPost) {
             if ($model->load($this->request->post()) && $model->save()) {
-                return $this->redirect(['view', 'jurusan_mapel_id' => $model->jurusan_mapel_id]);
+                return $this->redirect(['view', 'jurusan_id' => $model->jurusan_id]);
             }
         } else {
             $model->loadDefaultValues();
@@ -108,18 +83,18 @@ class JurusanMapelController extends Controller
     }
 
     /**
-     * Updates an existing JurusanMapel model.
+     * Updates an existing Jurusan model.
      * If update is successful, the browser will be redirected to the 'view' page.
-     * @param int $jurusan_mapel_id Jurusan Mapel ID
+     * @param int $jurusan_id Jurusan ID
      * @return string|\yii\web\Response
      * @throws NotFoundHttpException if the model cannot be found
      */
-    public function actionUpdate($jurusan_mapel_id)
+    public function actionUpdate($jurusan_id)
     {
-        $model = $this->findModel($jurusan_mapel_id);
+        $model = $this->findModel($jurusan_id);
 
         if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'jurusan_mapel_id' => $model->jurusan_mapel_id]);
+            return $this->redirect(['view', 'jurusan_id' => $model->jurusan_id]);
         }
 
         return $this->render('update', [
@@ -128,29 +103,29 @@ class JurusanMapelController extends Controller
     }
 
     /**
-     * Deletes an existing JurusanMapel model.
+     * Deletes an existing Jurusan model.
      * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param int $jurusan_mapel_id Jurusan Mapel ID
+     * @param int $jurusan_id Jurusan ID
      * @return \yii\web\Response
      * @throws NotFoundHttpException if the model cannot be found
      */
-    public function actionDelete($jurusan_mapel_id)
+    public function actionDelete($jurusan_id)
     {
-        $this->findModel($jurusan_mapel_id)->delete();
+        $this->findModel($jurusan_id)->delete();
 
         return $this->redirect(['index']);
     }
 
     /**
-     * Finds the JurusanMapel model based on its primary key value.
+     * Finds the Jurusan model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param int $jurusan_mapel_id Jurusan Mapel ID
-     * @return JurusanMapel the loaded model
+     * @param int $jurusan_id Jurusan ID
+     * @return Jurusan the loaded model
      * @throws NotFoundHttpException if the model cannot be found
      */
-    protected function findModel($jurusan_mapel_id)
+    protected function findModel($jurusan_id)
     {
-        if (($model = JurusanMapel::findOne(['jurusan_mapel_id' => $jurusan_mapel_id])) !== null) {
+        if (($model = Jurusan::findOne(['jurusan_id' => $jurusan_id])) !== null) {
             return $model;
         }
 
