e7ec1b40abbb81bdabe73072c1b14bc0bdf5fd32
Axel Sianturi <sianturiaxel@gmail.com>
2024-02-12 14:35:58 +0700

    update refrensi Agama

--- backend/views/agama/_form.php
+++ backend/views/agama/_form.php
@@ -8,16 +8,26 @@ use yii\widgets\ActiveForm;
 /** @var yii\widgets\ActiveForm $form */
 ?>
 
-<div class="agama-form">
-
+<div class="kode-ujian-form container mt-5">
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'desc')->textInput(['maxlength' => true]) ?>
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-4">
+                    <?= $form->field($model, 'desc')->textInput(['maxlength' => true]) ?>
+                </div>
+            </div>
+        </div>
+        <div class="card-footer">
+            <div class="form-group">
+                <?= Html::button('Kembali', ['class' => 'btn btn-warning', 'onclick' => 'history.go(-1)']) ?>
 
-    <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+                <?= Html::submitButton('Simpan', ['class' => 'btn btn-primary']) ?>
+            </div>
+
+        </div>
     </div>
 
     <?php ActiveForm::end(); ?>
-
 </div>
--- backend/views/agama/index.php
+++ backend/views/agama/index.php
@@ -7,38 +7,62 @@ use yii\grid\ActionColumn;
 use yii\grid\GridView;
 
 /** @var yii\web\View $this */
-/** @var backend\models\AgamaSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
-$this->title = 'Agamas';
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = 'Agama';
+$dataProvider->pagination = false;
+$createUrl = Url::to(['create']);
+$js = <<<JS
+$(document).ready(function() {
+    $(function () {
+        $('#datatables').DataTable({
+            'paging': true,
+            'lengthChange': false,
+            'searching': true,
+            'ordering': true,
+            'info': true,
+            'autoWidth': false,
+            'responsive': true,
+            'dom': "<'row'<'col-sm-12 col-md-4 toolbar'><'col-sm-12 col-md-8'f>>" + 
+               "<'row'<'col-sm-12'tr>>" + 
+               "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>"
+        })
+        $('div.toolbar').html('<a href=\"{$createUrl}\" class=\"btn btn-success\">Tambah Data Agama</a>');
+    });
+});
+JS;
+$this->registerJs($js);
 ?>
-<div class="agama-index">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Create Agama', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'agama_id',
-            'desc',
-            [
-                'class' => ActionColumn::className(),
-                'urlCreator' => function ($action, Agama $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'agama_id' => $model->agama_id]);
-                 }
-            ],
-        ],
-    ]); ?>
-
-
+<div class="card">
+    <div class="card-body">
+        <table id="datatables" class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Agama</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($dataProvider->getModels() as $index => $model) : ?>
+                    <tr>
+                        <td><?= $index + 1 ?></td>
+                        <td><?= Html::encode($model->desc) ?></td>
+                        <td>
+                            <?= Html::a('<i class="fa fa-eye"></i>', ['view', 'agama_id' => $model->agama_id], ['class' => 'btn btn-primary btn-sm', 'title' => 'View']) ?>
+                            <?= Html::a('<i class="fas fa-edit"></i>', ['update', 'agama_id' => $model->agama_id], ['class' => 'btn btn-info btn-sm', 'title' => 'Update']) ?>
+                            <?= Html::a('<i class="fa fa-trash"></i>', ['delete', 'agama_id' => $model->agama_id], [
+                                'class' => 'btn btn-danger btn-sm',
+                                'title' => 'Delete',
+                                'data' => [
+                                    'confirm' => 'Are you sure you want to delete this item?',
+                                    'method' => 'post',
+                                ],
+                            ]) ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 </div>
